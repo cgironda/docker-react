39c22ab7a5f806cceeae9eb167f84152c31af918
Carlos Gironda <cgironda@akane.waseda.jp>
2019-05-13 14:28:17 +0900

    My change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Greetings to Konica Minolta !!!
+          Konica Minolta !!!
         </p>
         <a
           className="App-link"
